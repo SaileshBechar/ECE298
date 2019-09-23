419cef18c3a7f956b53b776c401cd3386ec7792e
Sailesh Bechar <saileshbechar@yahoo.ca>
2019-09-23 19:56:18 -0400

    Included some libraries

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <msp430.h> 
-
+#include "main.h"
+#include "hal_LCD.h"
+#include "driverlib/driverlib.h"
 
 /**
  * main.c
@@ -7,6 +9,6 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+	  Init_LCD();     //Sets up the LaunchPad LCD display
 	return 0;
 }
